d580c65d25a41795f276e7a2ef324d383c8f3592
brendogomes <brendo.log@gmail.com>
2023-09-09 23:09:55 -0300

    feat add scope

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('../../modules/template/src/lib/template.module').then(
+        (m) => m.TemplateModule
+      ), 
+  },
+];
